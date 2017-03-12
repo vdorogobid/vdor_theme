8deb3486bbaed4606c0a920d87befa7e36ccbcc8
vdorogobid <vdorogobid@gmail.com>
2017-03-13 01:55:27 +0200

    Футер хедер

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="blog-footer">
+  <p>&copy; <?php echo Date ('Y'); ?> vdorogobid-theme <a href="https://www.facebook.com/vdorogobid">facebook</a></p>
+  <p>
+    <a href="#">Back to top</a>
+  </p>
+</footer>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
