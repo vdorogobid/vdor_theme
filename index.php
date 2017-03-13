de2d29d306fc3c1cbe402d7089d9f3587c8c8cef
vdorogobid <vdorogobid@gmail.com>
2017-03-13 15:59:05 +0200

    Подключил content.php

--- index.php
+++ index.php
@@ -13,19 +13,7 @@
         <div class="col-sm-8 blog-main">
           <?php if ( have_posts() ) : ?>
           <?php while ( have_posts() ) : the_post(); ?>
-          <div class="blog-post">
-            <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ;?></a></h2>
-            <p class="blog-post-meta"><?php the_date(); ?> by <a href="<?php the_author_link(); ?>">
-                            <?php the_author(); ?></a></p>
-            <?php if ( has_post_thumbnail()) :?>
-              <div class="post-thumb">
-                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" > </a>
-                    <?php the_post_thumbnail(); ?>
-              </div>
-        <?php endif; ?>
-            <?php the_excerpt(); ?>
-          </div><!-- /.blog-post -->
-
+          <?php get_template_part('content');?>
           <?php endwhile ?>
         <?php else : ?>
                       <p> <?php __('No posts'); ?></p>
