dbca4b82978258e2aa491c83cb6ac72162bd5ac2
vdorogobid <vdorogobid@gmail.com>
2017-03-13 17:10:15 +0200

    разные типы записей

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <div class="col-sm-8 blog-main">
           <?php if ( have_posts() ) : ?>
           <?php while ( have_posts() ) : the_post(); ?>
-          <?php get_template_part('content');?>
+          <?php get_template_part('content', get_post_format());?>
           <?php endwhile ?>
         <?php else : ?>
                       <p> <?php __('No posts'); ?></p>
@@ -22,6 +22,11 @@
 
 
         </div><!-- /.blog-main -->
+
+
+
+
+
 
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
           <div class="sidebar-module sidebar-module-inset">
