fccc126440d6d93519821d22fc676807cdecf514
vdorogobid <vdorogobid@gmail.com>
2017-03-13 12:17:54 +0200

    Подключил виджеты

--- functions.php
+++ functions.php
@@ -74,3 +74,40 @@ function set_excerpt_length(){
   return 35;
 }
 add_filter('excerpt_length', 'set_excerpt_length');
+
+// Widget Locations
+function wpb_init_widgets($id){
+  register_sidebar(array(
+    'name'  => 'Sidebar',
+    'id'    => 'sidebar',
+    'before_widget' => '<div class="sidebar-module">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>'
+  ));
+  register_sidebar(array(
+    'name'  => 'Box1',
+    'id'    => 'box1',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+  register_sidebar(array(
+    'name'  => 'Box2',
+    'id'    => 'box2',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+  register_sidebar(array(
+    'name'  => 'Box3',
+    'id'    => 'box3',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+}
+add_action('widgets_init', 'wpb_init_widgets');
