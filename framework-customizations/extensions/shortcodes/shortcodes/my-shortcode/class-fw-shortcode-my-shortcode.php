6c0bd4599825401d889d751a14ec69602651ba6a
vdorogobid <vdorogobid@gmail.com>
2017-04-09 18:11:40 +0300

    Создание нового шорткода: config file, создание класса.

---
+++ framework-customizations/extensions/shortcodes/shortcodes/my-shortcode/class-fw-shortcode-my-shortcode.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 28.03.17
+ * Time: 11:02 AM
+ */
+if (!defined('FW')) {
+    die('Forbidden');
+}
+class FW_Shortcode_My_Shortcode extends FW_Shortcode
+{
+
+    
+}
